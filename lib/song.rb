37702abe5317c1339583ebb7e5144e2fd4f42708
Jane Justiz <janejustiz@gmail.com>
2018-07-01 00:11:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,15 +16,15 @@ class Song
     @@genres << @genre
   end
 
-  def count
+  def self.count
     @@count
   end
 
-  def artists
+  def self.artists
     @@artists
   end
 
-  def genres
+  def self.genres
     @@genres
   end
 
