8090002cd26dbe75c17e3ee5b1148f17aec210f8
Jane Justiz <janejustiz@gmail.com>
2018-07-01 00:05:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,16 +2,34 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@artist = []
-  @@genre = []
+  @@artists = []
+  @@genres = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
-    @@artist << @artist
-    @@genre << @genre
+    @@artists << @artist
+    @@genres << @genre
   end
 
+  def count
+    @@count
+  end
+
+  def artists
+    @@artists
+  end
+
+  def genres
+    @@genres
+  end
+
+  def genre_count
+    
+  end
+
+  def artist_count
+  end
 end
